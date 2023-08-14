f68e2e788a67307af236738f647a4a4680e5d4e0
somnath07 <123575623+bsomnath07@users.noreply.github.com>
2023-08-14 15:00:32 +0530

    IMDB upload1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from './components/header/Header';
+import Home from './pages/home/home';
+import MovieList from './components/movieList/movieList';
+import Movie from './pages/movieDetail/movie';
+import Auth from './Auth';
+
+// function App() {
+
+//   return (
+//     <div className="App">
+    
+//  <Router>
+//           <Header />
+//             <Routes>
+//                  <Route index element={<Home />}></Route>
+//                 <Route path="movie/:id" element={<Movie/>}></Route>
+//                 <Route path="movies/:type" element={<MovieList/>}> </Route> 
+//                 <Route path="/*" element={<h1>Error Page</h1>}></Route>
+//                 <Route path="auth" element={<Auth />} />
+//             </Routes>
+//         </Router>
+//     </div>
+//   );
+// }
+
+
+// export default App;
 
 function App() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  // Function to update the login status
+  const handleLogin = () => {
+    setIsUserLoggedIn(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          {isUserLoggedIn ? (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/movie/:id" element={<Movie />} />
+              <Route path="/movies/:type" element={<MovieList />} />
+            </>
+          ) : (
+            <Route
+              path="/*"
+              element={<Auth onLogin={handleLogin} />} 
+            />
+          )}
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+// import IMDBlogo from "../images/IMDBlogo.jpg";
+// import IMDBlogo from "../../images/IMDBlogo.jpg";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerLeft">
+        <Link to="/">
+          {/* <img className="header_icon" src={IMDBlogo} alt="IMDb Logo" /> */}
+          <img
+            className="header__icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"
+          />
+        </Link>
+
+        <Link to="/movies/popular" style={{ textDecoration: "none" }}>
+          <span>Popular</span>
+        </Link>
+        <Link to="/movies/top_rated" style={{ textDecoration: "none" }}>
+          <span>TopRated</span>
+        </Link>
+        <Link to="/movies/upcoming" style={{ textDecoration: "none" }}>
+          <span>Upcoming</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
